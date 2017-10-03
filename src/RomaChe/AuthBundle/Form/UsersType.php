74fbc58742bfd028abf33386ce70579e01933b79
dydaev <dydaev@gmail.com>
2017-10-03 12:13:42 +0300

    added basic form for edit users

---
+++ src/RomaChe/AuthBundle/Form/UsersType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace RomaChe\AuthBundle\Form;
+
+use RomaChe\AuthBundle\Entity\Users;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\BooleanType;
+use Symfony\Component\Form\Extension\Core\Type\ArrayType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+class UsersType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+            ->add('dateAdd', DateTimeType::class)
+            ->add('roles')
+            ->add('birthday', BirthdayType::class)
+            ->add('sex', RadioType::class)
+            ->add('country', TextareaType::class)
+            ->add('city', TextareaType::class)
+            ->add('aboutSelf', TextareaType::class);
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Users::class
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'romache_authbundle_users';
+    }
+
+
+}
