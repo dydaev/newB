7f20c92f5ff4e283335ddbd138d6a697c437cf49
dydaev <dydaev@gmail.com>
2017-10-05 15:08:51 +0300

    initial react-redux and co.

---
+++ assets/consts/index.js
@@ -0,0 +1,16 @@
+const REDUCERS = {
+    NEWS: {
+        ADD: 'ADD_NEWS',
+        DELETE: 'DELETE_NEWS'
+    },
+    WEATHE: {
+        CHANGE_CITY: 'CHANGE_CITY'
+    },
+    AUTH: {
+        SUCCESS: 'AUTH_SUCCESS',
+        FAIL: 'AUTH_FAIL'
+    }
+};
+export const INCREASE = 'INCREASE';
+export const DECREASE = 'DECREASE';
+export default REDUCERS;
---
+++ assets/reducers/index.js
@@ -0,0 +1,13 @@
+
+export count from './count'
+
+/*
+import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+
+import authReducer from './auth.js';
+
+export default combineReducers({
+        routerReducer,
+        authReducer
+    });*/
