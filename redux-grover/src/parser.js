d37dbc1da563f10ec1367eac16d98ae58723ce78
dydaev <dydaev@gmail.com>
2017-11-07 00:07:53 +0200

    finish crate parser for redux-grover

--- redux-grover/src/parser.js
+++ redux-grover/src/parser.js
@@ -21,24 +21,102 @@ export const getObjectHeaderPosition = (grover, objectName) => {
   return headObjectPosition >= 0 ? headObjectPosition + 1 : false;
 };
 
+export const groverLength = (grover, groverName) =>
+getObjectEndPosition(grover, groverName) - getObjectHeaderPosition(grover, groverName) + 1;
+
+export const clearObjectInGrover = (grover, groverName) => {
+  const _position = getObjectHeaderPosition(grover, groverName) - 1;
+  const _length = groverLength(grover, groverName);
+  if (_position >= 0 && _length >= 1) {
+    grover.splice(_position, _length);
+  }
+
+  return grover;
+};
+
+export const preLineSpices = (grover, position) =>
+  grover[position].match(/(^[\s]*)/g).toString();
+
+export const update = (grover, objectName, objectBody) => {
+  let positionUpdateObject = (getObjectHeaderPosition(grover, objectName) - 1);
+  if (positionUpdateObject) {
+    grover = clearObjectInGrover(grover, objectName);
+  } else {
+    positionUpdateObject = nameAnalitic(grover, objectName);
+  }
+
+  const spices = preLineSpices(grover, positionUpdateObject);
+
+  if (!Array.isArray(objectBody)) objectBody = [objectBody];
+
+  objectBody.reverse();
+  objectBody.forEach(newLine => grover.splice(positionUpdateObject, 0, spices + newLine));
+
+  return grover;
+};
+
 export const getObject = (grover, objectName) =>
   grover.slice(
     getObjectHeaderPosition(grover, objectName) - 1,
     getObjectEndPosition(grover, objectName)
   );
 
-export const differentOfChar = (line, startChar, endChar) =>
-  (line.split(startChar).length - 1) -
-  (line.split(endChar).length - 1);
+const bordersOfObject  = [
+  { type: 'ob1', face: '\\s+function\\s+[a-z0-9]+(\\s)*\\(', _start: '{', _end: '}' },
+  { type: 'ob2', face: '\\s+const\\s+[a-z]+\\s*\\=\\s', _start: '{', _end: '}' },
+  { type: 'ob3', face: '\\s+case\\s', _start: '(', _end: ')' },
+  { type: 'arr1', face: '\\[', _start: '[', _end: ']' },
+  { type: 'default', face: '', _start: '{', _end: '}' },
+];
+
+export const differentOfChar = (line, objectType) =>
+(line.split(objectType._start).length - 1) -
+(line.split(objectType._end).length - 1);
 
 export const getObjectEndPosition = (grover, objectName) => {
-  const objectHead = getObjectHeaderPosition(grover, objectName);
-  let diffSimb = 0;
-  let inObj = false;
-  for (let ind = objectHead - 1; ind < grover.length; ind++) {
-    diffSimb = diffSimb + differentOfChar(grover[ind], '{', '}');
-    inObj = inObj ? inObj : diffSimb > 0;
-    if (inObj && diffSimb == 0) return ind + 1;
+  const objectHead = (getObjectHeaderPosition(grover, objectName) - 1);
+  if (objectHead < 0) {
+    console.log('did`nt find object name ', objectName);
+    return false;
   }
+
+  const objectType = bordersOfObject.find(({ face }) =>
+    new RegExp(face, 'i').test(grover[objectHead])
+  );
+
+  // console.log(objectType.type);
+  let diffSimb = 0;
+  let inObj = (grover[objectHead].split(objectType._start).length === 2
+  && grover[objectHead].split(objectType._end).length === 2);
+
+  for (let ind = objectHead; ind < grover.length; ind++) {
+    diffSimb = diffSimb + differentOfChar(grover[ind], objectType);
+    inObj = inObj ? inObj : diffSimb > 0;
+    if (inObj && diffSimb == 0)
+      return ind + 1;
+  }
+
   return false;
+};
+
+export const nameAnalitic = (grover, objectName) => {
+  let position = 1;
+  const res = grover.reduce((acc, lm, ind) => {
+    let isFirstWord = false;
+    const difference = objectName.split('_').filter((word, i) => {
+
+      if (new RegExp('(\\s|_|\.)' + word + '(_|\\s|\:)').test(lm)) {
+        isFirstWord = isFirstWord ? true : i === 0;
+        return true;
+      } else return false;
+    });
+
+    if (difference.length > acc && isFirstWord) {
+      acc = difference.length;
+      position = ind;
+    }
+
+    return acc;
+  }, 0);
+  return position;
 };
--- redux-grover/src/reducerMaker.js
+++ redux-grover/src/reducerMaker.js
@@ -1,5 +1,5 @@
 import * as folder from '../grover.config';
-import * as parserJSFile from './parser';
+import * as pars from './parser';
 
 var fs = require('fs');
 var path = require('path');
@@ -11,72 +11,15 @@ export const createReducer = (constName, stateSelector) => {
   const fileName = constName.split('_')[0].toLowerCase();
   const constsPath = folder.mainPath + folder.reducersPath + dirSeparator + fileName + '.js';
 
-  //TODO make scan dir for all reducers or read index file.
-  const grover = parserJSFile.getGrover(constsPath);
-  console.log('Open file ' + constsPath);
-  const asa = 'initialState';
-  console.log('start=', parserJSFile.getObjectHeaderPosition(grover, asa));
-  console.log('end=', parserJSFile.getObjectEndPosition(grover, asa));
-  console.log('object=', parserJSFile.getObject(grover, asa));
+  const grover = pars.getGrover(constsPath);
+  const obj = 'update';
+  console.log('Open file ' + constsPath + 'with object ' + obj);
+  //console.log('start=', pars.getObjectHeaderPosition(grover, obj));
+  //console.log('end=', pars.getObjectEndPosition(grover, obj));
 
-  // if (fs.existsSync(fileName)) {
-  //   if (getObjectHeaderPosition(constsPath, 'update')) {
-  //     if (!getObjectHeaderPosition(constsPath, 'STAFF_CHECKED_ADDING_ROLE')) {
-  //
-  //     }
-  //   } else {
-  //     console.log('no function update');
-  //   }
-  // }
+  let initState = pars.getObject(grover, obj);
+  console.log(initState);
 
-  fs.exists(constsPath, (exists) => {
-    if (exists) {
-      console.log('Open file ' + constsPath);
-
-      const file = fs.readFileSync(constsPath, 'utf8');
-      const fileArr = file.split(constNameChar);
-      const headConstPosition = fileArr.findIndex(lm => {
-        return new RegExp('\\s' + folder.constsName + '(\\s|\=)').test(lm);
-      });
-      const firstObjectPosition = fileArr.findIndex(lm => new RegExp('{').test(lm));
-      let position = headConstPosition >= 0 ? headConstPosition : firstObjectPosition;
-
-      if (headConstPosition < 0) {
-        fileArr.splice(
-          firstObjectPosition - 1,
-          0,
-          '',
-          `const ${folder.constsName} = {`,
-          '};',
-          `export default ${folder.constsName};`,
-          ''
-        );
-        position = firstObjectPosition;
-      } else {
-        const res = fileArr.reduce((acc, lm, ind) => {
-          let isFirstWord = false;
-          let difference = constName.split('_').filter((word, i) => {
-            if (new RegExp('(\\s|_)' + word + '(_|\\s|\:)').test(lm)) {
-              isFirstWord = isFirstWord ? true : i === 0;
-              return true;
-            }
-            return false;
-          });
-          if (difference.length > acc && isFirstWord) {
-            acc = difference.length;
-            position = ind - 1;
-          }
-          return acc;
-        }, 0);
-      }
-      if (position >= 0) {
-        fileArr.splice(position + 1, 0, `  ${constName}: '${constName}',`);
-      }
-
-      console.log(fileArr);
-    } else {
-      console.log('No file exist ' + constsPath);
-      return false;
-    }
-  });
+  initState = pars.update(initState, 'STAFF_GET_USER', ['NEW---LINE)','CINIMA']);
+  console.log(initState);
 };
