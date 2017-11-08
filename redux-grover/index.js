58847f710c1af769fa717d09612f66c2ee9b19f0
dydaev <dydaev@gmail.com>
2017-11-08 02:34:30 +0200

    create args worker

--- redux-grover/index.js
+++ redux-grover/index.js
@@ -5,13 +5,33 @@ import * as config from './grover.config';
 
 // constManager.readFolder();
 
-const constName  = 'STAFF_DELETE_NEW_ROLE';
-const route = '/api?role=update';
-const stateName = 'isRoleDelete';
+// const constName  = 'STAFF_DELETE_NEW_ROLE';
+// const route = '/api?role=update';
+// const stateName = 'isRoleDelete';
+if (process.argv.length === 6) {
+  const action = process.argv[2];
+  const constName  = process.argv[3];
+  const stateName = process.argv[4];
+  const route = process.argv[5];
 
-if (constName && route) routManager.createRoute(constName, route);
-if (constName) constManager.createConst(constName);
-if (constName && stateName) reducerMaker.createReducer(constName, stateName);
+  console.log(process.argv[2]);
+  if (action === 'UPDATE') {
+    // if (constName && route) routManager.createRoute(constName, route);
+    // if (constName) constManager.createConst(constName);
+    // if (constName && stateName) reducerMaker.createReducer(constName, stateName);
+  } else if (action === 'DELETE') {
+    console.log('in process writing)');
+  }
+} else if (process.argv.length === 3) {
+  if (process.argv[2] == '-h' ||
+  process.argv[2] == '-help' ||
+  process.argv[2] == '--h' ||
+  process.argv[2] == '--help') {
+    console.log("[UPDATE | DELETE] constName storeSelector route");
+  }
+} else {
+  console.log('Not params, for example: UPDATE STAFF_UPDATE_ROLE isStaffUpdated /api?staff=update_role');
+}
 
 //TODO generate dispatch(action)
 //TODO generate shortcut command for symfony controller generator
